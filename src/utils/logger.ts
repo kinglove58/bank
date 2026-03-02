7f117b94424c840aa96d159e0b9df4a7bee83944
kinglove58 <obafemielijahsunday@gmail.com>
2026-03-02 13:02:37 +0100

    feat: add morgan for logging HTTP requests and winston for structured logging
    - Added morgan middleware for logging HTTP requests in development mode.
    - Introduced winston for logging with file transports for error and combined logs.
    - Updated package.json and package-lock.json to include morgan and winston dependencies.
    - Created a logger utility using winston for consistent logging across the application.
    - Added indexes to the Transaction model for performance optimization in Prisma.

---
+++ src/utils/logger.ts
@@ -0,0 +1,29 @@
+import winston from "winston";
+
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json(),
+  ),
+  transports: [
+    //1. save all errors to a dedicated file
+    new winston.transports.File({ filename: "logs/error.log", level: "error" }),
+    //2. save all logs to a general file
+    new winston.transports.File({ filename: "logs/combined.log" }),
+  ],
+});
+
+if (process.env.NODE_ENV !== "production") {
+  logger.add(
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.simple(),
+      ),
+    }),
+  );
+}
+
+
+export default logger;
